97be0cb024de7f1ef420d06d028f20d460a1b247
moakajsa <grka24fe@student.ju.se>
2024-11-14 23:27:47 +0100

    addedScale

--- game.js
+++ game.js
@@ -2,12 +2,11 @@ function setup() {
   createCanvas(800, 600);
 }
 
-function draw() {
-  background(255, 255, 255);
-  package(300, 450);
-}
+let speed = 5;
+let down = -100;
+let slide = 100;
 
-function package(x, y) {
+function package(x, y, s) {
   background(255, 255, 255);
 
   let c = color(180, 30, 0);
@@ -21,29 +20,83 @@ function package(x, y) {
   push();
   fill(0, 0, 0);
   stroke(0, 0, 0);
-  strokeWeight(6);
+  strokeWeight(6 * s);
   strokeJoin(ROUND);
-  quad(x - 11, y, x - 70, y - 33, x - 70, y - 53, x - 11, y - 23);
+  quad(
+    x - 11 * s,
+    y,
+    x - 70 * s,
+    y - 33 * s,
+    x - 70 * s,
+    y - 53 * s,
+    x - 11 * s,
+    y - 23 * s
+  );
 
-  quad(x - 11, y + 100, x - 70, y + 63, x - 70, y - 30, x - 11, y + 2);
+  quad(
+    x - 11 * s,
+    y + 100 * s,
+    x - 70 * s,
+    y + 63 * s,
+    x - 70 * s,
+    y - 30 * s,
+    x - 11 * s,
+    y + 2 * s
+  );
   pop();
 
   // small part
-  quad(x - 11, y, x - 70, y - 33, x - 70, y - 53, x - 11, y - 23);
+  quad(
+    x - 11 * s,
+    y,
+    x - 70 * s,
+    y - 33 * s,
+    x - 70 * s,
+    y - 53 * s,
+    x - 11 * s,
+    y - 23 * s
+  );
 
-  quad(x - 11, y + 100, x - 70, y + 63, x - 70, y - 30, x - 11, y + 2);
+  quad(
+    x - 11 * s,
+    y + 100 * s,
+    x - 70 * s,
+    y + 63 * s,
+    x - 70 * s,
+    y - 30 * s,
+    x - 11 * s,
+    y + 2 * s
+  );
 
   // top
   push();
   fill(0, 0, 0);
   stroke(0, 0, 0);
-  strokeWeight(6);
+  strokeWeight(6 * s);
   strokeJoin(ROUND);
-  quad(x - 11, y - 25, x - 70, y - 55, x + 16, y - 82, x + 80, y - 55);
+  quad(
+    x - 11 * s,
+    y - 25 * s,
+    x - 70 * s,
+    y - 55 * s,
+    x + 16 * s,
+    y - 82 * s,
+    x + 80 * s,
+    y - 55 * s
+  );
 
   pop();
 
-  quad(x - 11, y - 25, x - 70, y - 55, x + 16, y - 82, x + 80, y - 55);
+  quad(
+    x - 11 * s,
+    y - 25 * s,
+    x - 70 * s,
+    y - 55 * s,
+    x + 16 * s,
+    y - 82 * s,
+    x + 80 * s,
+    y - 55 * s
+  );
 
   //right side
 
@@ -52,94 +105,197 @@ function package(x, y) {
   push();
   fill(0, 0, 0);
   stroke(0, 0, 0);
-  strokeWeight(6);
+  strokeWeight(6 * s);
   strokeJoin(ROUND);
-  quad(x - 10, y, x + 80, y - 33, x + 80, y - 53, x - 9, y - 23);
+  quad(
+    x - 10 * s,
+    y,
+    x + 80 * s,
+    y - 33 * s,
+    x + 80 * s,
+    y - 53 * s,
+    x - 9 * s,
+    y - 23 * s
+  );
 
-  quad(x - 10, y + 100, x + 80, y + 63, x + 80, y - 30, x - 9, y + 2);
+  quad(
+    x - 10 * s,
+    y + 100 * s,
+    x + 80 * s,
+    y + 63 * s,
+    x + 80 * s,
+    y - 30 * s,
+    x - 9 * s,
+    y + 2 * s
+  );
 
   pop();
-  quad(x - 9, y, x + 80, y - 33, x + 80, y - 53, x - 9, y - 23);
+  quad(
+    x - 9 * s,
+    y,
+    x + 80 * s,
+    y - 33 * s,
+    x + 80 * s,
+    y - 53 * s,
+    x - 9 * s,
+    y - 23 * s
+  );
 
-  quad(x - 9, y + 100, x + 80, y + 63, x + 80, y - 30, x - 9, y + 2);
+  quad(
+    x - 9 * s,
+    y + 100 * s,
+    x + 80 * s,
+    y + 63 * s,
+    x + 80 * s,
+    y - 30 * s,
+    x - 9 * s,
+    y + 2 * s
+  );
 
   // shadows
 
-  fill(160, 0, 0);
+  fill(100, 0, 0, 80);
   noStroke();
 
-  quad(x - 12, y + 10, x - 69, y + -22, x - 70, y - 30, x - 12, y + 2);
+  quad(
+    x - 10 * s,
+    y + 10 * s,
+    x - 70 * s,
+    y - 22 * s,
+    x - 70 * s,
+    y - 30 * s,
+    x - 10 * s,
+    y + 2 * s
+  );
 
-  quad(x - 8, y + 10, x + 80, y - 22, x + 80, y - 30, x - 8, y + 2);
+  quad(
+    x - 10 * s,
+    y + 10 * s,
+    x + 80 * s,
+    y - 22 * s,
+    x + 80 * s,
+    y - 30 * s,
+    x - 10 * s,
+    y + 2 * s
+  );
 
   //parachute
 
   //parachute strings
   stroke(0, 0, 0);
-  strokeWeight(3);
-  line(x, y - 50, x - 100, y - 190);
-  line(x, y - 50, x - 40, y - 190);
-  line(x, y - 50, x + 100, y - 190);
-  line(x, y - 50, x + 40, y - 190);
+  strokeWeight(3 * s);
+  line(x, y - 50 * s, x - 100 * s, y - 190 * s);
+  line(x, y - 50 * s, x - 40 * s, y - 190 * s);
+  line(x, y - 50 * s, x + 100 * s, y - 190 * s);
+  line(x, y - 50 * s, x + 40 * s, y - 190 * s);
 
   // balloon
-  fill(c);
+  fill(180, 30, 0);
   beginShape();
-  vertex(x - 100, y - 190);
-  bezierVertex(x - 100, y - 240, x - 40, y - 240, x - 40, y - 190);
-  bezierVertex(x - 40, y - 240, x + 40, y - 240, x + 40, y - 190);
-  bezierVertex(x + 40, y - 240, x + 100, y - 240, x + 100, y - 190);
-  bezierVertex(x + 100, y - 450, x - 100, y - 450, x - 100, y - 190);
+  vertex(x - 100 * s, y - 190 * s);
+  bezierVertex(
+    x - 100 * s,
+    y - 240 * s,
+    x - 40 * s,
+    y - 240 * s,
+    x - 40 * s,
+    y - 190 * s
+  );
+  bezierVertex(
+    x - 40 * s,
+    y - 240 * s,
+    x + 40 * s,
+    y - 240 * s,
+    x + 40 * s,
+    y - 190 * s
+  );
+  bezierVertex(
+    x + 40 * s,
+    y - 240 * s,
+    x + 100 * s,
+    y - 240 * s,
+    x + 100 * s,
+    y - 190 * s
+  );
+  bezierVertex(
+    x + 100 * s,
+    y - 450 * s,
+    x - 100 * s,
+    y - 450 * s,
+    x - 100 * s,
+    y - 190 * s
+  );
 
   endShape();
 
   //balloon pattern
   push();
-  fill(d);
+  fill(100, 0, 0, 90);
   noStroke();
   beginShape();
-  vertex(x - 40, y - 190);
-  bezierVertex(x - 40, y - 240, x + 40, y - 240, x + 40, y - 190);
-  bezierVertex(x + 70, y - 450, x - 70, y - 450, x - 40, y - 190);
+  vertex(x - 40 * s, y - 190 * s);
+  bezierVertex(
+    x - 40 * s,
+    y - 240 * s,
+    x + 40 * s,
+    y - 240 * s,
+    x + 40 * s,
+    y - 190 * s
+  );
+  bezierVertex(
+    x + 70 * s,
+    y - 450 * s,
+    x - 70 * s,
+    y - 450 * s,
+    x - 40 * s,
+    y - 190 * s
+  );
 
   endShape();
   pop();
 
   // left bow
-  fill(c);
+  fill(180, 30, 0);
   beginShape();
-  vertex(x, y - 55);
-  bezierVertex(x - 170, y - 150, x - 100, y - 40, x - 80, y - 30);
-  bezierVertex(x - 90, y - 20, x - 100, y + 130, x, y - 55);
+  vertex(x, y - 55 * s);
+  bezierVertex(
+    x - 170 * s,
+    y - 150 * s,
+    x - 100 * s,
+    y - 40 * s,
+    x - 80 * s,
+    y - 30 * s
+  );
+  bezierVertex(x - 90 * s, y - 20 * s, x - 100 * s, y + 130 * s, x, y - 55 * s);
   endShape();
 
   // right bow
 
   beginShape();
-  vertex(x, y - 55);
-  bezierVertex(x + 140, y - 220, x + 100, y - 70, x + 80, y - 65);
-  bezierVertex(x + 100, y - 70, x + 180, y + 80, x, y - 55);
+  vertex(x, y - 55 * s);
+  bezierVertex(
+    x + 140 * s,
+    y - 220 * s,
+    x + 100 * s,
+    y - 70 * s,
+    x + 80 * s,
+    y - 65 * s
+  );
+  bezierVertex(x + 100 * s, y - 70 * s, x + 180 * s, y + 80 * s, x, y - 55 * s);
   endShape();
 
   //middle
-  fill(d);
-  ellipse(x, y - 55, 30);
+  fill(150, 0, 0);
+  ellipse(x, y - 55 * s, 30 * s);
 }
 
-let speed = 5;
-let down = -100;
-let slide = 100;
-
 function draw() {
-  package(slide + 100, down);
+  package(slide + 100, down, 1);
 
   if (keyIsDown(39)) {
     slide = slide + speed;
   } else if (keyIsDown(37)) {
     slide = slide - speed;
   }
-
-  if (down <= 400) {
-    down = down + 2;
-  }
+  if (down < 400) down = down + 2;
 }
