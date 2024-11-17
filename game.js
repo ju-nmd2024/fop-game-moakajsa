c1093d72a2578089528f2444802ceaa615fc6e2b
moakajsa <grka24fe@student.ju.se>
2024-11-17 14:55:13 +0100

    winAndLose

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let state = "start";
 
-let gameState = true;
+// let gameState = true;
 
 let packageY = 100;
 let packageX = 100;
@@ -10,7 +10,7 @@ let acceleration = 0.2;
 
 let slide = 10;
 
-let bagX = 100;
+let bagX = random(width);
 let bagY = 370;
 
 function setup() {
@@ -474,26 +474,30 @@ function gameScreen() {
   package(packageX + slide, packageY, 0.5);
   bagFront(bagX, bagY, 1);
 
-  if (gameState === true) {
-    // gravity logic
-    packageY = packageY + velocityY;
-    velocityY = velocityY + acceleration;
+  // if (gameState === true) {
+  // gravity logic
+  packageY = packageY + velocityY;
+  velocityY = velocityY + acceleration;
 
-    if (keyIsDown(37)) {
-      slide = slide - acceleration * 15;
-    } else if (keyIsDown(39)) {
-      slide = slide + acceleration * 15;
-    }
-
-    // decrease the velocity pressing up button
-    if (keyIsDown(38)) {
-      velocityY = velocityY - 0.5;
-    }
-
-    if (packageY >= 700) {
-      gameState = false;
-    }
+  if (keyIsDown(37)) {
+    slide = slide - acceleration * 15;
+  } else if (keyIsDown(39)) {
+    slide = slide + acceleration * 15;
   }
+
+  // decrease the velocity pressing up button
+  if (keyIsDown(38)) {
+    velocityY = velocityY - 0.5;
+  }
+
+  if (
+    packageY >= 700 &&
+    packageX + slide >= bagX &&
+    packageX + slide <= bagX + 75
+  ) {
+    // gameState = true;
+    console.log(" YOU WIN!");
+  } else if (packageY >= 700) console.log("YOU LOOSE!");
 }
 
 function resultScreen() {
